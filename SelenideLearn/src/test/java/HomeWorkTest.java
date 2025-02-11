24840a188f94e13bfb20d73825d71b7f5875d60c
TatKarpova <arnfread@gmail.com>
2025-02-11 19:16:47 +0300

    HomeWork. The first attempt

--- SelenideLearn/src/test/java/HomeWorkTest.java
+++ SelenideLearn/src/test/java/HomeWorkTest.java
@@ -1,4 +1,80 @@
-package PACKAGE_NAME;
+import com.codeborne.selenide.*;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
-public class HomeWorkTest {
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+
+public class HomeworkTest {
+    public static void addDevice(SelenideElement device){
+        device.click();
+        device.should(Condition.disappear);
+        $(byText("Add to cart")).shouldBe(Condition.visible)
+                .click();
+        Selenide.confirm();
+
+        $(byText("Home ")).click();
+    }
+
+    @Test
+    void selenideTest(){
+        Selenide.open("https://demoblaze.com");
+        ElementsCollection cards = Selenide.elements(Selectors.byClassName("card"));
+        cards.should(CollectionCondition.size(9));
+
+        SelenideElement nokiaLumiaCard = cards.find(Condition.text("Nokia lumia 1520"));
+        SelenideElement nokiaLumiaPriceText = nokiaLumiaCard.find(By.tagName("h5"));
+        int nokiaPrice =Integer.parseInt(nokiaLumiaPriceText.getText());
+        addDevice(nokiaLumiaCard);
+
+        SelenideElement iphone = cards.find(Condition.text("Iphone 6 32gb"));
+        SelenideElement iphonePriceText = iphone.find(By.tagName("h5"));
+        int iphonePrice =Integer.parseInt(iphonePriceText.getText());
+        addDevice(iphone);
+
+        SelenideElement sonyVaioI7 = cards.find(Condition.text("Sony vaio i7"));
+        SelenideElement sonyPriceText = sonyVaioI7.find(By.tagName("h5"));
+        int sonyPrice =Integer.parseInt(sonyPriceText.getText());
+        addDevice(sonyVaioI7);
+
+        SelenideElement cart = Selenide.element(Selectors.byId("cartur"));
+        cart.click();
+
+        int total = Integer.parseInt(Selenide.element(Selectors.byId("totalp")).getText());
+        if (total!=(nokiaPrice+iphonePrice+sonyPrice)){
+            System.out.println("The total amount of the basket is incorrect");
+        }
+        $(byText("Place Order")).click();
+
+        SelenideElement nameInput = Selenide.element(Selectors.byId("name"));
+        SelenideElement countryInput = Selenide.element(Selectors.byId("country"));
+        SelenideElement cityInput = Selenide.element(Selectors.byId("city"));
+        SelenideElement cardInput = Selenide.element(Selectors.byId("card"));
+        SelenideElement monthInput = Selenide.element(Selectors.byId("month"));
+        SelenideElement yearInput = Selenide.element(Selectors.byId("year"));
+
+        nameInput.setValue("test");
+        countryInput.setValue("test");
+        cityInput.setValue("test");
+        cardInput.setValue("test");
+        monthInput.setValue("may");
+        yearInput.setValue("2025");
+        $(byText("Purchase")).click();
+
+        SelenideElement info=Selenide.element(By.className("lead"));
+        String[] wholeText = info.getText().split("\n");
+        String id = wholeText[0].substring(3);
+        String[] finalPrice = wholeText[1].split(" ");
+        int price = Integer.parseInt(finalPrice[1]);
+        if (price!=(nokiaPrice+iphonePrice+sonyPrice)){
+            System.out.println("The total amount of the basket is incorrect");
+        }
+        System.out.println("Order "+ id);
+
+        Selenide.confirm();
+        Selenide.element(By.className("sweet-alert"))
+                .should(Condition.disappear);
+
+    }
+
 }
--- untitled2/src/test/java/WebUI/WebUITest.java
+++ untitled2/src/test/java/WebUI/WebUITest.java
@@ -1,4 +1,68 @@
 package WebUI;
 
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+
 public class WebUITest {
+    String userLogin ="standard_user";
+    String password ="secret_sauce";
+
+    @BeforeAll
+    public static void setUp(){
+        Selenide.open("https://www.saucedemo.com");
+    }
+    @Test
+    public void loginTest(){
+        //Проверяем, что зашли на страницу
+        $(".login_logo").shouldHave(text("Swag Labs"));
+        //Ищем и вводим данные для входа
+        $("#user-name").setValue(userLogin);
+        $("#password").setValue(password);
+
+        $("#login-button").click();
+        //Проверяем переход на страницу
+        $(".title").shouldBe(visible).shouldHave(text("Products"));
+        sleep(3000);
+    }
+
+   /* @Test
+    public void addToCard(){
+        //Находи 1 карточку
+        SelenideElement item =$("(//div[@class='inventory_item_description'])[1]");
+        item.shouldBe(visible);
+        //Проверяем имя карточки
+        SelenideElement itemName = $("#item_4_title_link");
+        itemName.$(".inventory_item_name ").shouldHave(text("Sauce Labs Backpack"));
+        //Добывляем товар в корзину
+        item.$("#add-to-cart-sauce-labs-backpack").click();
+        //Проверяем, что товар добавился
+        item.$("#remove-sauce-labs-backpack").shouldBe(visible);
+        SelenideElement shoppingCart =$(".shopping_cart_link");
+        shoppingCart.$("data-test='shopping-cart-badge'").shouldBe(visible).shouldHave(text("1"));
+    }*/
+
+    @Test
+    public void detailedСard(){
+        //Находи 1 карточку
+        SelenideElement item =$x("//body/div[@id='root']/div[@id='page_wrapper']/div[@id='contents_wrapper']/div[@id='inventory_container']/div/div[@id='inventory_container']/div[@class='inventory_list']/div[1]");
+        item.shouldBe(visible);
+        //Проверяем имя карточки
+        SelenideElement itemName = $("#item_4_title_link");
+        itemName.$(".inventory_item_name ").shouldHave(text("Sauce Labs Backpack"));
+        //Переходим в карточку товара
+        itemName.click();
+        //Проверяем карточку товара
+        $("#back-to-products").shouldBe(visible);
+        $(".inventory_details_name").shouldBe(visible)
+                .shouldHave(text("Sauce Labs Backpack"));
+
+    }
+
+
 }
